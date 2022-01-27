e99313bf99d34748893a116cbb287e4ccb12dacb
Pedro Vinicius M. S. Rodrigues <pdrfps@gmail.com>
2022-01-27 11:48:59 -0300

    Initial commit

---
+++ pyhton/src/estudo.py
@@ -0,0 +1,16 @@
+print("*******************************")
+print("Bem-vindo ao jogo de advinhacao!")
+print("*******************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero")
+
+print("Voce digitou" , chute)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print("Voce acertou")
+else:   
+    print("Voce errou")
