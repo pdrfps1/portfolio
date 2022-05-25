6d24c2aa5d25ad0f2f92ce60ab3ced8c188ecd4d
Pedro Vinicius M. S. Rodrigues <pdrfps@gmail.com>
2022-05-25 15:02:42 -0300

    Atualização
    Modificado estilos da página.

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".switch");
+const theme = document.querySelector("#theme-link");
+btn.addEventListener("click", 
+function(){
+    if (theme.getAttribute("href") == "../styles/dark-mode.css"){
+        theme.href = "../styles/light-mode.css";
+    }else{
+        theme.href = "../styles/dark-mode.css";
+    }
+});
